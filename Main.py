49c72953ee39a3921d97047a520be488ebc05b87
RobertHumolli <120424157+RobertHumolli@users.noreply.github.com>
2023-10-28 15:18:19 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
+import pygame,sys
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+
+screen = pygame.display.set_mode((300,600)) #This sets the display for the game
+pygame.display.set_caption("Tetris")
+
+clock = pygame.time.Clock()
+
+while True:         #This is the main game loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            pygame.quit()               #This will exit the display once the game is finished
+            sys.exit()
+
+    screen.fill(dark_blue) #Colour can be updated
+
+    pygame.display.update()         #This will update the display
+    clock.tick(60)                  #This sets the ticks for the game, which I will be running it at 60 frames per second
 
